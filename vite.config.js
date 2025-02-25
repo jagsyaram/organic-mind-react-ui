0ba0111182b9b0ee018598e92022e5244f8ff040
Jagadeeswara Reddy Yaramala <jagsyaram@gmail.com>
2025-02-26 00:02:22 +0530

    After removing CDNs for Tailwind

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   base: "https://github.com/jagsyaram/organic-mind-react-ui", // Replace 'your-repo-name' with the name of your GitHub repository
 });
