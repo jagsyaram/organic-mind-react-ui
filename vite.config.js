ef9751c0b20022f3e91650243bd12ee4b72c8646
Jagadeeswara Reddy Yaramala <jagsyaram@gmail.com>
2025-02-25 23:38:32 +0530

    Github Pages publish First run

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: "https://github.com/jagsyaram/organic-mind-react-ui", // Replace 'your-repo-name' with the name of your GitHub repository
+});
